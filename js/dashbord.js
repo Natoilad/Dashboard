f860efbd4c901f085ee89f792fa66f8c7e1e2204
Natoilad <109770565+Natoilad@users.noreply.github.com>
2023-06-17 00:54:44 +0300

    js

--- js/dashbord.js
+++ js/dashbord.js
@@ -1,15 +1,13 @@
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector("[data-menu-open]"),
-    closeMenuBtn: document.querySelector("[data-menu-close]"),
-    menu: document.querySelector("[data-menu]"),
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
   };
 
-  refs.openMenuBtn.addEventListener("click", toggleModal);
-  refs.closeMenuBtn.addEventListener("click", toggleModal);
+  refs.openMenuBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    document.body.classList.toggle("modal-open");
-    refs.menu.classList.toggle("is-open");
+    refs.menu.classList.toggle('is-open');
   }
 })();
